35fd9a0f75ae386320dec276c46a103988928350
Taylor Sanders <tsanders3515@gmail.com>
2021-02-17 23:05:07 -0900

    adding section3 html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-console.log(process.argv)
+//WHEN THE USER SCROLLS THE PAGE, EXECUTE myFunction
+window.onscroll = function () { myFunction() };
+
+//GET THE NAVBAR
+var navbar = document.getElementById("navbar");
+
+//GET THE OFFSET POSITION OF THE NAVBAR
+var sticky = navbar.offsetTop;
+
+//ADD THE STICKY CLASS TO THE NAVBAR WHEN YOU REACH ITS SCROLL POSITION. REMOVE "STICKY" WHEN YOU LEAVE THE SCROLL POSITION
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
