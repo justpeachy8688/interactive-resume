dd5e93d675bd2bafb6647ac76aa928a05f86ffb2
Taylor Sanders <tsanders3515@gmail.com>
2021-04-15 21:49:30 -0800

    adding portfolio projects

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 //WHEN THE USER SCROLLS THE PAGE, EXECUTE myFunction
-window.onscroll = function () { myFunction() };
+window.onscroll = function () {
+  myFunction();
+};
 
 //GET THE NAVBAR
 var navbar = document.getElementById("navbar");
@@ -9,11 +11,11 @@ var sticky = navbar.offsetTop;
 
 //ADD THE STICKY CLASS TO THE NAVBAR WHEN YOU REACH ITS SCROLL POSITION. REMOVE "STICKY" WHEN YOU LEAVE THE SCROLL POSITION
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
 
 // function getCurrentDate() {
